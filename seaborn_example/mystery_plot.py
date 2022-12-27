268b04d9f37689dffb5da840ab048aa7a5ac1d78
DhiaZ <dhia.zerrari@outlook.com>
2022-12-27 15:30:41 +0100

    retired unused files

--- seaborn_example/mystery_plot.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-#TODO fix dataset filename not being found depending on VSCode workspace
-dataFrame = pd.read_csv('seaborn_example/mystery_dataset.csv')
-dataFrame['Vavg (Volts)'] = (dataFrame['V1 (Volts)'] + dataFrame['V2 (Volts)'] + dataFrame['V3 (Volts)']) / 3.0
-sns.lineplot(x = "Time interval (seconds)", y = "Vavg (Volts)", data=dataFrame)
-plt.show()
